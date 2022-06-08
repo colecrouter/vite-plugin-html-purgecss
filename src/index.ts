db22c57ccab6d43c24a95fba40bf59af70751196
Mexican-Man <colecrouter@hotmail.com>
2022-06-08 17:32:21 -0600

    Changed options to use UserDefinedOptions instead of UserDefinedSafelist

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import PurgeCSS, { UserDefinedSafelist } from "purgecss";
+import PurgeCSS, { UserDefinedOptions } from "purgecss";
 
-export default (safeList?: UserDefinedSafelist) => {
+export default (options?: Partial<UserDefinedOptions>) => {
     let _html: string = '';
     return {
         name: 'vite-plugin-html-purgecss',
         enforce: 'post',
-        transformIndexHtml(html) { _html += html;},
+        transformIndexHtml(html) { _html += html; },
         async generateBundle(_options, bundle) {
             const cssFiles = Object.keys(bundle).filter(key => key.endsWith('.css'));
             if (!cssFiles) return;
             for (const file of cssFiles) {
                 const purged = await new PurgeCSS().purge({
-                    content: [{raw: _html, extension: 'html'}],
-                    css: [{raw: bundle[file].source}],
-                    safelist: safeList || []
+                    ...options,
+                    content: [{ raw: _html, extension: 'html' }],
+                    css: [{ raw: bundle[file].source }],
                 });
                 bundle[file].source = purged[0].css;
             }
